ad48fe14fbdceaebec53396c6195ef1ee7c91545
azharul77 <suvromulla921@gmail.com>
2025-04-26 11:46:05 +0600

    temparature conversion and calendar program added

---
+++ problem_7.py
@@ -0,0 +1,5 @@
+# temparature conversion
+celsius = float(input("Enter the temperature in Celsius: "))
+
+fahrenheit = (celsius * 9/5) + 32
+print(f"The {celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
---
+++ problem_8.py
@@ -0,0 +1,9 @@
+# display clendar
+
+import calendar
+
+year = int(input("Enter the year: "))
+month = int(input("Enter the month: "))
+
+cal = calendar.month(year, month)
+print(cal)
